5d7254ddcc87f2535767ebef7b2b50f4592a6239
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2023-02-08 08:26:07 +0100

    Updated github pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/js/game.ts',
+	entry: './src/game/game.ts',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: "bundle.js",
